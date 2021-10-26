8ddec30b97c8316b4a86446e458f16c2da1e71dc
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-10-26 19:54:10 +0530

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React, { Fragment } from "react";
+import { Switch, Route } from "react-router-dom";
+
+import AboutUs from "./pages/AboutUs";
 import HomePage from "./pages/HomePage";
+import Testimonials from "./pages/Testimonials";
+import Destinations from "./pages/Destinations";
+
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/destinations">
+          <Destinations />
+        </Route>
+        <Route exact path="/about-us">
+          <AboutUs />
+        </Route>
+        <Route exact path="/testimonials">
+          <Testimonials />
+        </Route>
+      </Switch>
+      <Footer />
+    </Fragment>
   );
 }
 
