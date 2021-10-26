5b254389d93156a4536e753cc8e0157dbba11e9c
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-10-26 11:54:38 +0530

    Removed unused imports

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,14 +3,12 @@ import Header from "../components/Header";
 import CTA from "../components/CTA";
 import Footer from "../components/Footer";
 import Places from "../components/Places";
-import Testimonial from "../components/Testimonial";
 
 const HomePage = () => {
   return (
     <div>
       <Header />
       <Places />
-      {/* <Testimonial /> */}
       <CTA />
       <Footer />
     </div>
